2323b13d9acfebcfd6d5549cde38eb7951631216
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-11-17 15:12:53 +0000

    管理者ログイン画面(途中)

--- db/schema.rb
+++ db/schema.rb
@@ -42,9 +42,9 @@ ActiveRecord::Schema.define(version: 2023_11_17_063130) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
-    t.string "name", default: "", null: false
-    t.string "postcode", default: "", null: false
-    t.string "address", default: "", null: false
+    t.string "name", null: false
+    t.string "postcode", null: false
+    t.string "address", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -75,13 +75,13 @@ ActiveRecord::Schema.define(version: 2023_11_17_063130) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "last_name", default: "", null: false
-    t.string "first_name", default: "", null: false
-    t.string "last_name_kana", default: "", null: false
-    t.string "first_name_kana", default: "", null: false
-    t.string "postcode", default: "", null: false
-    t.string "address", default: "", null: false
-    t.string "telephone_number", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "postcode", null: false
+    t.string "address", null: false
+    t.string "telephone_number", null: false
     t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -90,15 +90,15 @@ ActiveRecord::Schema.define(version: 2023_11_17_063130) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name", default: "", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "items", force: :cascade do |t|
     t.integer "genre_id", null: false
-    t.string "name", default: "", null: false
-    t.text "detail", default: "", null: false
+    t.string "name", null: false
+    t.text "detail", null: false
     t.integer "price", null: false
     t.boolean "is_sales_status", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
@@ -121,9 +121,9 @@ ActiveRecord::Schema.define(version: 2023_11_17_063130) do
     t.integer "order_status", default: 0, null: false
     t.integer "billing_amount", null: false
     t.integer "postage", null: false
-    t.string "name", default: "", null: false
-    t.string "postcode", default: "", null: false
-    t.string "address", default: "", null: false
+    t.string "name", null: false
+    t.string "postcode", null: false
+    t.string "address", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
