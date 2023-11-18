ff0218bb2f282ce2677dc65fc50373365fab7d91
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-11-18 14:55:38 +0000

    顧客会員画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,6 +14,9 @@ class Admin::CustomersController < ApplicationController
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
+      redirect_to admin_customers_path(@customer.id)
+    else
+      render :edit
     end
   end
 end
