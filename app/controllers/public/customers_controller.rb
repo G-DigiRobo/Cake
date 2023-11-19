1d15adf9b0d350f4ab718e1f2cf111549889fb0d
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-11-19 08:56:25 +0000

    会員情報

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,20 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def confirm
+    @customer = Customer.find(params[:id])
   end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to
+    else
+      render :edit
 end
