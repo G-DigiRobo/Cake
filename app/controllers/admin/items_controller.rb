54ae7f6837b7cd94064aad3ca94ec801429bdfd2
machii0601 <144580610+machii0601@users.noreply.github.com>
2023-11-20 09:04:46 +0000

    admin/itemの新規投稿

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,7 +4,29 @@ class Admin::ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:notice] = "投稿できました"
+      redirect_to admin_item_path(@item)
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "投稿できました"
+      redirect_to admin_items_path
+    else
+      @items = Item.all
+      render :new
+    end
+  end
+
   def new
+    @item = Item.new
   end
 
   def show
@@ -12,5 +34,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :detail, :price, :is_sales_status)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
   validates :name, presence: true
   validates :detail, presence: true
+
+  belongs_to :genre
+  has_one_attached :image
+
+  enum is_sales_status: {
+    "販売中":0, "販売停止中":1
+  }
+
+  def get_profile_image(width, height)
+  unless image.attached?
+    file_path = Rails.root.join('app/assets/images/cake_b.png')
+    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 end
