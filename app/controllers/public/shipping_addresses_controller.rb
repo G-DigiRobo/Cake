c0dd1dee6b711b0189b5816c50f68cc02d070e77
ayakatonton <145669983+ayakatonton@users.noreply.github.com>
2023-11-20 14:13:06 +0000

    haisousaki.index

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,7 +1,47 @@
 class Public::ShippingAddressesController < ApplicationController
-  def index
-  end
+  before_action :find_address, only: [:edit, :update, :destroy]
+   
+    def new
+      @shipping_address = ShippingAddress.new
+    end
+    
+    def index
+      @shipping_addresses = current_customer.shipping_addresses 
+    end
+  
+    def edit
+    end
+    
+    def create
+      @shipping_address = ShippingAddress.new(shipping_address_params)
+      @shipping_address.customer = current_customer
+      
+      if @shipping_address.save
+        redirect_to @shipping_address, notice: 'Shipping address was successfully created.'
+      else
+        render :new
+      end
+    end
+ 
+    def update
+      if @shipping_address.update(shipping_address_params)
+        redirect_to @shipping_address, notice: 'Shipping address was successfully updated.'
+      else
+        render :edit
+      end
+    end
 
-  def edit
-  end
+    def destroy
+      @shipping_address.destroy
+      redirect_to shipping_addresses_url, notice: 'Shipping address was successfully destroyed.'
+    end
+    
+  private
+    def find_address
+      @shipping_address = current_customer.shipping_addresses.find(params[:id]) 
+    end
+    
+    def shipping_address_params
+      params.require(:shipping_address).permit(:recipient_name, :address_line_1, :address_line_2, :city, :state, :postal_code, :country)
+    end
 end
