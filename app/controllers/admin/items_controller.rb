e0cd2503689c8d4abe4af7bcf511940fda27b60c
machii0601 <144580610+machii0601@users.noreply.github.com>
2023-11-20 03:38:23 +0000

    pullしたいだけ

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,14 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @item = Item.new
+    @items = Item.all
   end
 
   def new
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
