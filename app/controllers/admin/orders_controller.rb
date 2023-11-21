8fb1e7933c6630facb99c3e3105e51c664b77b41
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-11-21 09:35:16 +0000

    管理者側の注文詳細画面ハリボテ

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,13 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+  end
+  
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+    else
+      
+    end
   end
 end
