5a2da9df78a481b76dc7270bc2ba0d68d4df1bb5
EC2 Default User <ysk.ymtk0613@gmail.com>
2023-11-21 13:23:34 +0000

    enum登録

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
   belongs_to :customer
 
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum order_status: { awaiting_payment: 0, payment_confirmation: 1, under_manufacture: 2, preparing_to_ship: 3, shipped: 4 }
 end
