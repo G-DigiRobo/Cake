2f3d3582ca75608956818e4e7be3cd9a6738a9f4
EC2 Default User <ysk.ymtk0613@gmail.com>
2023-11-25 15:34:42 +0000

    fix

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,13 +2,15 @@ class Public::SearchesController < ApplicationController
   def search
     @genres = Genre.all
     @range = params[:range]
-    @items = Item.looks(params[:word])
+    @word = params[:word]
+    @items = Item.looks(@word).page(params[:page]).per(8)
   end
 
   def genre_search
     @genre_id = params[:genre_id]
+    @genre = Genre.find_by(id: @genre_id)
     @genres = Genre.all
     @range = params[:range]
-    @items = Item.where(genre_id: @genre_id)
+    @items = Item.where(genre_id: @genre_id).page(params[:page]).per(8)
   end
 end
