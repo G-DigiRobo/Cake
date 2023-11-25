6fe8a5b82888321e8763b2acac0cc1d23009f3d7
EC2 Default User <ysk.ymtk0613@gmail.com>
2023-11-25 12:46:46 +0000

    検索欄に特定ワードを入力すると管理者ログインページへ遷移

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,5 +1,10 @@
 class Public::SearchesController < ApplicationController
   def search
+    @genres = Genre.all
+    @range = params[:range]
     @items = Item.looks(params[:word])
+    if params[:word] == 'adminsignin'
+      redirect_to new_admin_session_path
+    end
   end
 end
